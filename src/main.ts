c13663c2c08b601224e1d0d569e657d86d9d5351
himkwtn <him10424@gmail.com>
2019-09-06 23:35:14 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new WsAdapter(app));
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
