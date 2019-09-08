74ac0e3dd7cb5070cda290279e676de025c148c8
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-09-08 21:00:29 +0700

    :bug: fix socket url

--- client/src/app/core/service/web-socket.service.ts
+++ client/src/app/core/service/web-socket.service.ts
@@ -1,40 +1,43 @@
 import { Injectable, Inject } from '@angular/core';
 import {
-  WebSocketSubjectConfig,
-  WebSocketSubject,
-  webSocket
+    WebSocketSubjectConfig,
+    WebSocketSubject,
+    webSocket,
 } from 'rxjs/webSocket';
 import { ENV } from './environment.service';
 import {
-  WebSocketEvent,
-  WebSocketIncomingEvent
+    WebSocketEvent,
+    WebSocketIncomingEvent,
 } from '../models/web-socket.model';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class WebSocketService {
-  connection: WebSocketSubject<WebSocketEvent<any>>;
+    connection: WebSocketSubject<WebSocketEvent<any>>;
 
-  constructor(@Inject(ENV) env: any) {
-    this.connection = webSocket<WebSocketEvent<any>>(env.socketUrl);
-  }
+    constructor(@Inject(ENV) env: any) {
+        const url = env.production
+            ? `wss://${window.location.hostname}/`
+            : env.socketUrl;
+        this.connection = webSocket<WebSocketEvent<any>>(url);
+    }
 }
 
 export const filterEvent = (event: WebSocketIncomingEvent) => (
-  source: Observable<WebSocketEvent<any>>
+    source: Observable<WebSocketEvent<any>>,
 ) => {
-  return source.pipe(
-    filter(evt => evt.event === event),
-    map(e => {
-      switch (e.event) {
-        case WebSocketIncomingEvent.connected:
-          return e.data as [string];
-        default:
-          return e.data;
-      }
-    })
-  );
+    return source.pipe(
+        filter(evt => evt.event === event),
+        map(e => {
+            switch (e.event) {
+                case WebSocketIncomingEvent.connected:
+                    return e.data as [string];
+                default:
+                    return e.data;
+            }
+        }),
+    );
 };
