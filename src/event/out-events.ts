e63c697169785238ffc46a6f524ea14536adc07b
himkwtn <him10424@gmail.com>
2019-09-10 11:49:06 +0700

    refactor(event)

--- src/event/out-events.ts
+++ src/event/out-events.ts
@@ -1,10 +1,13 @@
-import { PlayerInfo } from '../models/player';
+import { PlayerInfo, ChatMessage } from '../models/player';
 
 export const enum OUT_EVENT {
     CONNECTED = 'CONNECTED',
     PLAYER_INFO = 'PLAYER_INFO',
+    CHAT_MESSAGE = 'CHAT_MESSAGE',
 }
 
 export type ConnectedEvent = PlayerInfo[];
 
 export type NewPlayerInfoEvent = PlayerInfo;
+
+export type ChatMessageEvent = ChatMessage;
