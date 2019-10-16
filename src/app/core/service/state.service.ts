52fe054b4eb4f76f6b9a07d0066aa7142dd258b6
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-10-16 14:03:52 +0700

    :construction: add simple state

---
+++ src/app/core/service/state.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Machine, interpret } from 'xstate';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class StateService {
+    static machine = Machine({
+        id: 'light',
+        initial: 'idle',
+        context: {
+            elapsed: 0,
+            direction: 'east',
+        },
+        states: {
+            idle: {
+                on: {
+                    READY: 'ready',
+                },
+            },
+            ready: {
+                on: {
+                    IDLE: 'idle',
+                },
+            },
+            playing: {
+                initial: 'playing',
+                states: {
+                    playing: {
+                        on: {
+                            ANSWERED: 'waiting',
+                        },
+                    },
+                    waiting: {
+                        on: {
+                            ROUND_END: 'endRound',
+                        },
+                    },
+                    endRound: {
+                        on: {
+                            NEXT_ROUND: 'playing',
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    service = interpret(StateService.machine);
+
+    constructor() {}
+}
